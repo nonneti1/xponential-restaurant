c3c878be99499cdac17711503f98e518ce0e6448
Netiphong <netiphong.a@giantwhalestudio.com>
2024-03-30 19:29:09 +0700

    feat: set globalPipelines, interceptors main app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document);
 
+  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalInterceptors(new TransformInterceptor(new Reflector()))
   await app.listen(3000);
 }
 bootstrap();
