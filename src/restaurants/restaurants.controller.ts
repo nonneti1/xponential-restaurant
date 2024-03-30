6e15d6fcbc2c1c2df7cc3dadd09d15fd85ad70b6
Netiphong <netiphong.a@giantwhalestudio.com>
2024-03-30 20:24:35 +0700

    feat: added response detail in restaurant controller

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Body, Controller, Post, Res } from '@nestjs/common
 import { RestaurantService } from './restaurants.service';
 import { InitRestaurantDto } from './dto/restaurants.dto'
 import { ResponseMessage } from '../decorators/response_message.decorator';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('/restaurants')
 export class RestaurantsController {
@@ -15,6 +16,8 @@ export class RestaurantsController {
     */
     @Post("/init")
     @ResponseMessage('Restaurant initialized')
+    @ApiResponse({ status: 400, description: "error initializing restaurant" })
+    @ApiResponse({ status: 201, description: 'successfully initializing restaurant' })
     init(@Body() initRestaurantDto: InitRestaurantDto) {
         try {
             this.restaurantService.initRestaurant(initRestaurantDto);
