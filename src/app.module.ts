979203d8711276ca2be8df18fc2d8b748f69ac75
Netiphong <netiphong.a@giantwhalestudio.com>
2024-03-30 20:23:53 +0700

    feat: remove unused api in app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { RestaurantsController } from './restaurants/restaurants.controller';
@@ -9,7 +8,7 @@ import { ReservationsController } from './reservations/reservations.controller';
 
 @Module({
   imports: [RestaurantsModule, ReservationsModule, ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController, RestaurantsController, ReservationsController],
+  controllers: [RestaurantsController, ReservationsController],
   providers: [AppService],
 })
 export class AppModule { }
