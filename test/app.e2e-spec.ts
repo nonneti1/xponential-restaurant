6d3543893dddfa8dddfd3a3d8d3b5859cb0152ba
Netiphong <netiphong.a@giantwhalestudio.com>
2024-03-30 19:30:41 +0700

    test: added e2e test for main app

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { TransformInterceptor } from '../src/interceptors/response.interceptor';
+import { Reflector } from '@nestjs/core';
 
-describe('AppController (e2e)', () => {
+describe('End-to-End testing', () => {
   let app: INestApplication;
+  let appHttpServer = null;
+  let bookingId = "";
+  const validRestaurantBody = { tableAmount: 5 };
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalInterceptors(new TransformInterceptor(new Reflector()))
     await app.init();
+    appHttpServer = app.getHttpServer();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  it('Open restaurant with 5 tables', async () => {
+    const test = await request(appHttpServer).post('/restaurants/init').send(validRestaurantBody).expect(201)
+
+    expect(test.body.message).toEqual("Restaurant initialized")
+    expect(test.body.data).toStrictEqual({})
+  })
+
+  it('Reservation for 5 customers', async () => {
+    const test = await request(appHttpServer).post('/reservations/reserve').send({ customerAmount: 5 }).expect(201)
+
+    expect(test.body.message).toEqual("Reservation created")
+    expect(typeof test.body.data.bookingId).toBe("string")
+    expect(test.body.data.reserveTables).toEqual(2)
+    expect(test.body.data.remainingTables).toEqual(3)
+  })
+
+  it('Reservation for 10 customers', async () => {
+    const test = await request(appHttpServer).post('/reservations/reserve').send({ customerAmount: 10 }).expect(201)
+    bookingId = test.body.data.bookingId;
+    expect(test.body.message).toEqual("Reservation created")
+    expect(typeof test.body.data.bookingId).toBe("string")
+    expect(test.body.data.reserveTables).toEqual(3)
+    expect(test.body.data.remainingTables).toEqual(0)
+  })
+
+  it('Cancel last reservation', async () => {
+    const test = await request(appHttpServer).post('/reservations/cancel').send({ bookingId: bookingId }).expect(201)
+
+    expect(test.body.message).toEqual("Reservation cancelled")
+    expect(test.body.data.freedTables).toEqual(3)
+    expect(test.body.data.remainingTables).toEqual(3)
+  })
+
 });
