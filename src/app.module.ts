3c69292acb6a0d43d6bf3fe6a18fed35bfa5ac49
Netiphong <netiphong.a@giantwhalestudio.com>
2024-03-30 19:28:18 +0700

    feat: updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { RestaurantsController } from './restaurants/restaurants.controller';
+import { ReservationsModule } from './reservations/reservations.module';
+import { ConfigModule } from '@nestjs/config';
+import { ReservationsController } from './reservations/reservations.controller';
 
 @Module({
-  imports: [RestaurantsModule],
-  controllers: [AppController, RestaurantsController],
+  imports: [RestaurantsModule, ReservationsModule, ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [AppController, RestaurantsController, ReservationsController],
   providers: [AppService],
 })
 export class AppModule { }
