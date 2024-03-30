537faabe70f8da9c2b8014e926edf9632f43a4a8
Netiphong <netiphong.a@giantwhalestudio.com>
2024-03-30 19:25:38 +0700

    feat: implemented restaurant controller api

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, Res } from '@nestjs/common';
 import { RestaurantService } from './restaurants.service';
+import { InitRestaurantDto } from './dto/restaurants.dto'
+import { ResponseMessage } from '../decorators/response_message.decorator';
 
 @Controller('/restaurants')
 export class RestaurantsController {
     constructor(private readonly restaurantService: RestaurantService) { }
 
-    @Post("/init")
-    init() {
-        // this.restaurantService.initRestaurant()
-        return "Initialized restaurant"
+    /* POST API enpoint to initialize the restaurant 
+    Requires body {
+        tableAmount:number
     }
-
+    return empty data and response message
+    */
+    @Post("/init")
+    @ResponseMessage('Restaurant initialized')
+    init(@Body() initRestaurantDto: InitRestaurantDto) {
+        try {
+            this.restaurantService.initRestaurant(initRestaurantDto);
+            return {}
+        } catch (error) {
+            throw new BadRequestException('Error while initializing restaurant', { cause: error, description: `BadRequest ${error}` })
+        }
+    }
 }
